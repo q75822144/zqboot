d56a29ad4dea128091dc27c04a9dbdaca3064aef
zhouquan <1260095732@qq.com>
2017-11-24 18:07:41 +0800

    编译跳过test

--- src/test/java/com/zqboot/ZqbootApplicationTests.java
+++ src/test/java/com/zqboot/ZqbootApplicationTests.java
@@ -2,7 +2,6 @@ package com.zqboot;
 
 import com.zqboot.common.RedisService;
 import com.zqboot.constant.RedisConstant;
-import com.zqboot.utils.SpringUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,20 +12,20 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ZqbootApplicationTests {
 
-//    @Autowired
-//    private RedisService redisService;
+    @Autowired
+    private RedisService redisService;
 
     @Test
     public void contextLoads() {
     }
 
-//    @Test
-//    public void redisadd() {
-//        boolean res = redisService.set(RedisConstant.TOKEN + "test", "123456");
-//        System.out.println(res);
-//        System.out.println(redisService.get(RedisConstant.TOKEN + "test"));
-////        RedisService redisService = SpringUtils.getBeanByType(RedisService.class);
-////        System.out.println(redisService);
-//    }
+    @Test
+    public void redisadd() {
+        boolean res = redisService.set(RedisConstant.TOKEN + "test", "123456");
+        System.out.println(res);
+        System.out.println(redisService.get(RedisConstant.TOKEN + "test"));
+//        RedisService redisService = SpringUtils.getBeanByType(RedisService.class);
+//        System.out.println(redisService);
+    }
 
 }
