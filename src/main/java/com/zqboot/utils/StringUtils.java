78f013a4deb55152967de04dd4c26b101a534868
zhouquan <1260095732@qq.com>
2017-12-04 14:19:55 +0800

    配置修改、工具类修改

--- src/main/java/com/zqboot/utils/StringUtils.java
+++ src/main/java/com/zqboot/utils/StringUtils.java
@@ -1,7 +1,5 @@
 package com.zqboot.utils;
 
-import sun.misc.BASE64Decoder;
-
 import java.io.Serializable;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
@@ -140,34 +138,6 @@ public class StringUtils {
     }*/
     public static String trimToEmpty(Object str){
         return (isBlank(str) ? "" : str.toString().trim());
-    }
-
-    /**
-     * 将 Strig  进行 BASE64 编码
-     * @param str [要编码的字符串]
-     * @param bf  [true|false,true:去掉结尾补充的'=',false:不做处理]
-     * @return
-     */
-    public static String getBASE64(String str,boolean...bf) {
-        if (StringUtils.isBlank(str)) return null;
-        String base64 = new sun.misc.BASE64Encoder().encode(str.getBytes()) ;
-        //去掉 '='
-        if(isBlank(bf) && bf[0]){
-            base64 = base64.replaceAll("=", "");
-        }
-        return base64;
-    }
-
-    /** 将 BASE64 编码的字符串 s 进行解码**/
-    public static String getStrByBASE64(String s) {
-        if (isBlank(s)) return "";
-        BASE64Decoder decoder = new BASE64Decoder();
-        try {
-            byte[] b = decoder.decodeBuffer(s);
-            return new String(b);
-        } catch (Exception e) {
-            return "";
-        }
     }
 
     /**
