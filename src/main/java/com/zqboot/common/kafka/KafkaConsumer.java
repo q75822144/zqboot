a362288fa5feea225cde069cabdfe960b6aa1eb4
zhouquan <1260095732@qq.com>
2019-04-12 17:03:37 +0800

    kafka集成消息队列

--- src/main/java/com/zqboot/common/kafka/KafkaConsumer.java
+++ src/main/java/com/zqboot/common/kafka/KafkaConsumer.java
@@ -1,5 +1,7 @@
 package com.zqboot.common.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -8,14 +10,17 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class KafkaConsumer {
+    private static Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
 
     /**
      * 监听主题,有消息就读取
+     *
      * @param message 数据包
      */
     @KafkaListener(topics = {"zhou"})
-    public void receiveMessage(String message){
-        System.out.println("收到消息"+message);
+    public void receiveMessage(String message) {
+        logger.info("11111111");
+        System.out.println("收到消息" + message);
         // 后续操作待完善
     }
 }
